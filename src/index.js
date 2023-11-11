804a8733117874c0b53444782bdfa16bc41c6fbc
Sharath Raj <sharath.r01@testyantra.in>
2023-11-11 09:41:12 +0530

    s

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import RactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let name='sharath';
+RactDOM.render(React.createElement('h1',null,React.createElement('p',id='1')),document.getElementById('root'));//throw react
